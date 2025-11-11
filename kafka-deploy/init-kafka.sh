c20fec05c8778fd43a99be056ddd7caad5312d84
satishthapak <satishthapak@outlook.com>
2025-11-11 14:35:11 +0530

    Trigger build

--- kafka-deploy/init-kafka.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Waiting for Kafka to be ready..."
-sleep 10
-
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 \
-  --create --if-not-exists --topic demo-topic \
-  --partitions 1 --replication-factor 1
-
-echo "Topic 'demo-topic' created."
